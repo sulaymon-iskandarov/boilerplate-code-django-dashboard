fdfb236a6890736eaed742978090f051bbe6d39a
koladev <onaelmangabo@gmail.com>
2021-09-05 13:17:33 +0100

    - refactoring: better naming

--- apps/app/config.py
+++ apps/app/config.py
@@ -8,4 +8,4 @@ from django.apps import AppConfig
 
 class MyConfig(AppConfig):
     name = 'apps.app'
-    label= 'apps_app'
+    label = 'apps_app'
--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -8,9 +8,6 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.models import User
-from django.forms.utils import ErrorList
-from django.http import HttpResponse
 from .forms import LoginForm, SignUpForm
 
 

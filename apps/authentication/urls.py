a8c69cfcef93981099b636a5d7e9a9aa91fc987e
koladev <onaelmangabo@gmail.com>
2021-09-15 12:47:41 +0100

    - feat: add password reset urls

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -5,10 +5,16 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django.urls import path
 from .views import login_view, register_user
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth import views
 
 urlpatterns = [
+
     path('login/', login_view, name="login"),
     path('register/', register_user, name="register"),
-    path("logout/", LogoutView.as_view(), name="logout")
+    path("logout/", views.LogoutView.as_view(), name="logout"),
+    path('password_reset/', views.PasswordResetView.as_view(), name="password_reset"),
+    path('reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('password_reset/done/', views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('reset/done/', views.PasswordResetCompleteView.as_view(), name="password_reset_complete")
+
 ]

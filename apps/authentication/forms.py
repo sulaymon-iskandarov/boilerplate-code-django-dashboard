e2e02834f1f333db92d1d6fddf32b40b3955d45a
koladev <onaelmangabo@gmail.com>
2021-09-17 01:48:42 +0100

    - feat: extend usercreation form

--- apps/authentication/forms.py
+++ apps/authentication/forms.py
@@ -2,10 +2,14 @@
 """
 Copyright (c) 2019 - present AppSeed.us
 """
-
+from django.conf import settings
+from django.core.mail import send_mail
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from apps.authentication.token import account_activation_token
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
 
 
 class LoginForm(forms.Form):
@@ -58,3 +62,26 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+
+        if not settings.EMAIL_CONFIRMATION:
+            if commit:
+                user.save()
+            return user
+
+        user.is_active = False
+
+        if commit:
+            user.save()
+
+        # Sending email
+        token = account_activation_token.make_token(user)
+        uid = urlsafe_base64_encode(force_bytes(user.pk))
+        subject = 'Activate your account'
+        message = f"This is your activation link. " \
+                  f"http://localhost:8002/activate/{uid}/{token}/"
+        send_mail(subject, message, settings.EMAIL_HOST_USER, [user.email])
+        return user

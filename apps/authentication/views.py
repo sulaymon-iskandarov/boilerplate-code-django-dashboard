0abe06165d4a5210403d29fa243d459df172ed42
koladev <onaelmangabo@gmail.com>
2021-09-17 01:48:59 +0100

    - feat: add activate user view

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -7,7 +7,11 @@ Copyright (c) 2019 - present AppSeed.us
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm, SignUpForm
-
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.utils.encoding import force_text
+from django.utils.http import urlsafe_base64_decode
+from apps.authentication.token import account_activation_token
 
 def login_view(request):
     form = LoginForm(request.POST or None)
@@ -54,3 +58,21 @@ def register_user(request):
         form = SignUpForm()
 
     return render(request, "accounts/register.html", {"form": form, "msg": msg, "success": success})
+
+
+def activate_account(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        msg = 'Your account have been confirmed.'
+        return render(request, 'registration/activation.html', {"msg": msg, "success": True})
+    else:
+        msg = 'The confirmation link was invalid, possibly because it has already been used.'
+        return render(request, 'registration/activation.html', {"msg": msg, "success": False})

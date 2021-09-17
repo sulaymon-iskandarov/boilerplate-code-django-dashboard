f7e46398f1fc9405f17b877a41a2f43affd1993b
koladev <onaelmangabo@gmail.com>
2021-09-17 01:04:36 +0100

    - feat: token account activation token generator

---
+++ apps/authentication/token.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+
+
+class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
+    def _make_hash_value(self, user, timestamp):
+        login_timestamp = '' if user.last_login is None else user.last_login.replace(microsecond=0, tzinfo=None)
+        return f"{user.pk}{user.username}{login_timestamp}{timestamp}"
+
+
+account_activation_token = AccountActivationTokenGenerator()

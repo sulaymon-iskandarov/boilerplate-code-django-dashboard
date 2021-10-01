bf576f7fa8c67fc3d18529da6ebe07484692676c
Kolawole Mangabo <onaelmangabo@gmail.com>
2021-10-01 23:06:43 +0100

    - feat: add user_photo field on form

--- apps/profile/forms.py
+++ apps/profile/forms.py
@@ -5,7 +5,9 @@ from apps.profile.models import Profile
 
 class ProfileForm(forms.ModelForm):
 
+    user_photo = forms.ImageField(required=False)
+
     class Meta:
         model = Profile
         fields = ('birthday', 'gender', 'phone', 'address', 'address_number',
-                  'city', 'zip', 'first_name', 'last_name', 'state')
+                  'city', 'zip', 'first_name', 'last_name', 'state', 'country', 'user_photo')

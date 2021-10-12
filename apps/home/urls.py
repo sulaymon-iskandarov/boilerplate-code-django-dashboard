47c574c0e820dba24375dbb893f48cf79d1df301
Kolawole Mangabo <onaelmangabo@gmail.com>
2021-10-12 23:10:32 +0100

    - fix: #24: change settings links

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
 
     # The home page
     path('', views.index, name='home'),
-    path('settings/', profile_view, name="profile"),
+    path('settings/', profile_view, name="settings"),
 
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),

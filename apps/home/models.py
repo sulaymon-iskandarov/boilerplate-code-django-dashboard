dd75e733fef6ce6ba584e8b1b830824a3121b253
Kolawole Mangabo <onaelmangabo@gmail.com>
2021-10-28 07:41:42 +0100

    - feat: add Data model

--- apps/home/models.py
+++ apps/home/models.py
@@ -4,7 +4,11 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.db import models
-from django.contrib.auth.models import User
 
-# Create your models here.
 
+class Data(models.Model):
+
+    type = models.CharField(max_length=35, choices=[('product','product'), ('transaction', 'transaction')])
+    name = models.CharField(max_length=35)
+    value = models.IntegerField()
+    ts = models.DateTimeField(auto_now=True)

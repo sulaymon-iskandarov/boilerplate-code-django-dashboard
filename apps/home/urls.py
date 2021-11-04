0e8a47a789d181aee29158c904d2f8c07c5d219d
Kolawole Mangabo <onaelmangabo@gmail.com>
2021-11-04 15:12:23 +0100

    - feat:wip: fix transaction template

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -6,12 +6,14 @@ Copyright (c) 2019 - present AppSeed.us
 from django.urls import path, re_path
 from apps.home import views
 from apps.profile.views import profile_view
+from apps.datatables.views import TransactionView
 
 urlpatterns = [
 
     # The home page
     path('', views.index, name='home'),
     path('settings/', profile_view, name="settings"),
+    path('transactions/', TransactionView.as_view(), name="transactions"),
 
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),

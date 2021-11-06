52123d003134807c447a830e1f1df89abcad53cc
Kolawole Mangabo <onaelmangabo@gmail.com>
2021-11-06 13:14:43 +0100

    - feat: fix url

--- apps/datatables/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import re_path
-from apps.datatables.views import TransactionView
-
-urlpatterns = [
-
-    re_path(r'^transactions/(?:(?P<pk>\d+)/)?(?:(?P<action>\w+)/)?', TransactionView.as_view(),
-            name='transactions'),
-]
--- core/urls.py
+++ core/urls.py
@@ -12,5 +12,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),          # Django admin route
     path("", include("apps.authentication.urls")), # Auth routes - login / register
     path("", include("apps.home.urls")),             # UI Kits Html files
-    path("", include("apps.datatables.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

50398b3b474b1ba285fcfcd80dd1ec9514821e3c
koladev <onaelmangabo@gmail.com>
2021-11-27 11:41:02 +0100

    - fix: #30: changing field order

--- apps/datatables/forms.py
+++ apps/datatables/forms.py
@@ -4,12 +4,14 @@ from apps.home.models import Data
 
 
 class TransactionForm(forms.ModelForm):
-
     name = forms.CharField(label="Name", widget=forms.TextInput(attrs={'class': 'form-control transaction'}))
-
     value = forms.DecimalField(label="Total", max_digits=10, decimal_places=2,
                                widget=forms.TextInput(
                                    attrs={'class': 'form-control transaction', 'placeholder': '...'}))
+    ts = forms.DateField(label="Timestamp", widget=forms.DateInput(
+        attrs={'class': 'form-control datepicker_input transaction', 'placeholder': 'yyyy-mm-dd'}), disabled=True)
+
+    field_order = ('id', 'name', 'ts', 'value')
 
     class Meta:
         model = Data

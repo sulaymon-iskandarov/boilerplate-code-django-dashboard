4b160a443eb3bef4a1a7b1ca336a014384265b8c
App Generator <51070104+app-generator@users.noreply.github.com>
2022-06-06 16:55:54 +0300

    CleanUp before update

--- apps/datatables/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class DatatablesConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.datatables"
-    label = "apps_datatables"
--- apps/profile/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from apps.profile.models import Profile
-
-admin.site.register(Profile)
